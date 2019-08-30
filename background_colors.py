fca25818a3b88d1f38bed442872c859698bd1887
BruceMustache <viniciusxnicolas007@gmail.com>
2019-08-30 20:55:17 -0300

    working with colors and adding background colors

---
+++ background_colors.py
@@ -0,0 +1,9 @@
+from color_functions import background
+
+black = background(0)
+red = background(1)
+green = background(2)
+yellow = background(3)
+pink = background(4)
+cyan = background(5)
+
---
+++ testing_colors.py
@@ -0,0 +1,4 @@
+for counter in range(7):
+    print(f'\033[4{counter}m' + ' ' * 8)
+print('\033[m')
+
