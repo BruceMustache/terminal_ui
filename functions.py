2d3c94bc29957a104a0616e0af6dd0c89005f9fe
BruceMustache <viniciusxnicolas007@gmail.com>
2019-08-30 22:41:47 -0300

    making a small animation and changing the display work flow

--- functions.py
+++ functions.py
@@ -1,7 +1,6 @@
 import color_functions
 
 
-DISPLAY_MATRIX = []
 DISPLAY_WIDTH = None
 DISPLAY_HEIGHT = None
 
@@ -9,27 +8,30 @@ DISPLAY_HEIGHT = None
 def update():
     show_display_matrix()
 
-def show_display_matrix():
-    global DISPLAY_MATRIX, DISPLAY_HEIGHT, DISPLAY_WIDTH
+def show_display_matrix(display):
+    global DISPLAY_HEIGHT, DISPLAY_WIDTH
+    # TODO put moving command in the display
     for row_counter in range(DISPLAY_HEIGHT):
         for column_counter in range(DISPLAY_WIDTH):
-            print(DISPLAY_MATRIX[row_counter][column_counter], end='')
-        print()
-    print( color_functions.reset(), end='' )
+            print(display[row_counter][column_counter], end='')
+        print(color_functions.reset())
 
     """ I try but something get wrong
-    for row in DISPLAY_MATRIX:
+    for row in display_matrix:
         for element in row:
             print(row, end='')
         print()
     """
 
 def create_display(width, height, default_value):
-    global DISPLAY_MATRIX, DISPLAY_WIDTH, DISPLAY_HEIGHT
+    global display_matrix, DISPLAY_WIDTH, DISPLAY_HEIGHT
     DISPLAY_WIDTH = width
     DISPLAY_HEIGHT = height
+
+    display_matrix = []
     for width_counter in range(height):
         row = [default_value] * width
-        DISPLAY_MATRIX.append(row + [color_functions.reset()])
+        display_matrix.append(row)
+    return display_matrix
 
 
--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from functions import *
 import background_colors as background
+import time
 
-create_display(5, 3, background.red+' ')
-show_display_matrix()
+
+def clear():
+    import os
+    os.system('clear')
+
+display = create_display(5, 3, background.red+' ')
+clear()
+show_display_matrix(display)
+time.sleep(1)
+display[1][1] = background.pink + ' '
+clear()
+show_display_matrix(display)
+time.sleep(1)
+display[1][2] = background.pink + ' '
+clear()
+show_display_matrix(display)
 
