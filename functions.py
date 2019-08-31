1a83ebbf814ee47ed31280dcf18c86c19f9cbcef
BruceMustache <viniciusxnicolas007@gmail.com>
2019-08-30 21:00:11 -0300

    adding create display function

--- functions.py
+++ functions.py
@@ -6,8 +6,14 @@ def update():
 
 def show_display_matrix():
     global DISPLAY_MATRIX
+    for row in DISPLAY_MATRIX:
+        for element in row:
+            print(row, end='')
+        print()
 
 def create_display(width, hegith, default_value):
-
+    for width_counter in range(width):
+        row = [default_value] * width
+        DISPLAY_MATRIX.append(row)
 
 
