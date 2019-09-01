4a963d16d9c3dd1950fec2385704cf1cfb72d81a
BruceMustache <viniciusxnicolas007@gmail.com>
2019-09-01 14:59:43 -0300

    adding white color

--- background_colors.py
+++ background_colors.py
@@ -1,9 +1,11 @@
-from color_functions import background
+from color_functions import background, color
 
+white = color(7)
 black = background(0)
 red = background(1)
 green = background(2)
 yellow = background(3)
-pink = background(4)
-cyan = background(5)
+blue = background(4)
+pink = background(5)
+cyan = background(6)
 
