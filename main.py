b7b2a1e88cb36a8c6ac044b5cba386ecee1a4fd9
BruceMustache <viniciusxnicolas007@gmail.com>
2019-09-01 14:36:41 -0300

    modularizando funcao afim

--- main.py
+++ main.py
@@ -10,27 +10,22 @@ def clear():
     os.system('clear')
 
 def funcao_afim(a, b, display):
-    print(MIDDLE_X)
-    for x in range(-1*MIDDLE_X, 0, -1):
-        y = a*x +  b
-        cord = position_functions.cartesian_position(x, y, display)
-        display[cord[1]][cord[0]] = background.yellow + ' '
-        time.sleep(0.1)
-        show_display_matrix(display)
+	MIDDLE_X = position_functions.MIDDLE_X
+	MIDDLE_Y = position_functions.MIDDLE_Y
+	a = 1
+	b = 0
+	for x in range(-1*MIDDLE_X, MIDDLE_X):
+		y = a*x + b
+		if abs(y) >= MIDDLE_Y or abs(x) >= MIDDLE_X:
+			continue
+		cord = position_functions.cartesian_position(x, y, display)
+		display[cord[1]][cord[0]] = background.yellow + ' '
+		time.sleep(0.3)
+		clear()
+		show_display_matrix(display)
 
-display = create_display(80, 20, background.red+' ')
+
+display = create_display(80, 20, background.green+' ')
 position_functions.set_variables(display)
-MIDDLE_X = position_functions.MIDDLE_X
-MIDDLE_Y = position_functions.MIDDLE_Y
-a = 1 
-b = 0
-for x in range(-1*MIDDLE_X, MIDDLE_X):
-    y = a*x + b
-    if abs(y) >= MIDDLE_Y or abs(x) >= MIDDLE_X:
-        continue
-    cord = position_functions.cartesian_position(x, y, display)
-    display[cord[1]][cord[0]] = background.yellow + ' '
-    time.sleep(0.3)
-    clear()
-    show_display_matrix(display)
+funcao_afim(1, 2, display)
 
