210a0f426dc8e58dc7423ef18072e80710215b14
BruceMustache <viniciusxnicolas007@gmail.com>
2019-09-01 14:43:18 -0300

    remmoving animation

--- main.py
+++ main.py
@@ -9,23 +9,21 @@ def clear():
     # print('\n' * 100)
     os.system('clear')
 
-def funcao_afim(a, b, display):
+def funcao_afim(a, b, inputedColor, display):
 	MIDDLE_X = position_functions.MIDDLE_X
 	MIDDLE_Y = position_functions.MIDDLE_Y
-	a = 1
-	b = 0
 	for x in range(-1*MIDDLE_X, MIDDLE_X):
 		y = a*x + b
+		# TODO limit dont get to the end or begin
 		if abs(y) >= MIDDLE_Y or abs(x) >= MIDDLE_X:
 			continue
 		cord = position_functions.cartesian_position(x, y, display)
-		display[cord[1]][cord[0]] = background.yellow + ' '
-		time.sleep(0.3)
-		clear()
-		show_display_matrix(display)
+		display[cord[1]][cord[0]] = inputedColor + ' '
 
 
 display = create_display(80, 20, background.green+' ')
 position_functions.set_variables(display)
-funcao_afim(1, 2, display)
+funcao_afim(0, 0, background.red, display)
+funcao_afim(1, 2, background.yellow, display)
+show_display_matrix(display)
 
